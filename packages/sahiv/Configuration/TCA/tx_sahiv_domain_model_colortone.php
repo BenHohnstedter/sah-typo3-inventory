bf442316cb5fd5f976989164cf821b44afb4cd08
BenHohnstedter <bhohnstedter@gmail.com>
2025-08-25 20:00:09 +0200

    Add additional tca models

---
+++ packages/sahiv/Configuration/TCA/tx_sahiv_domain_model_colortone.php
@@ -0,0 +1,82 @@
+<?php
+
+defined('TYPO3') or die;
+
+$ll = 'LLL:EXT:sahiv/Resources/Private/Language/locallang_db.xlf:';
+$model = 'tx_sahiv_domain_model_colortone';
+
+return [
+    'ctrl' => [
+        'title' => $ll . $model,
+        'label' => 'title',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'delete' => 'deleted',
+        'sortby' => 'sorting',
+        'default_sortby' => 'title',
+        'versioningWS' => true,
+        'rootLevel' => -1,
+        'typeicon_classes' => [
+            'default' => 'ext-sahiv-color',
+        ],
+        'languageField' => 'sys_language_uid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'translationSource' => 'l10n_source',
+        'searchFields' => 'title',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+            'starttime' => 'starttime',
+            'endtime' => 'endtime',
+        ],
+        'security' => [
+            'ignorePageTypeRestriction' => true,
+        ],
+    ],
+    'columns' => [
+        'title' => [
+            'exclude' => false,
+            'label' => $ll . $model . '.title',
+            'config' => [
+                'type' => 'input',
+                'size' => 30,
+                'required' => true,
+                'eval' => 'unique,trim',
+            ],
+        ],
+        'pearls' => [
+            'exclude' => true,
+            'label' => $ll . $model . '.pearls',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectMultipleSideBySide',
+                'foreign_table' => 'tx_sahiv_domain_model_pearl',
+                'MM' => 'tx_sahiv_pearls_colortones_mm',
+                'MM_opposite_field' => 'pearls',
+                'maxitems' => 10,
+            ],
+        ],
+        'hidden' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'default' => 0,
+            ],
+        ],
+    ],
+    'types' => [
+        0 => [
+            'showitem' => '--div--;General, --palette--;;palette_general, pearls, --palette--;;palette_settings',
+        ],
+    ],
+    'palettes' => [
+        'palette_general' => [
+            'showitem' => 'title',
+        ],
+        'palette_settings' => [
+            'showitem' => 'hidden',
+        ],
+    ],
+];

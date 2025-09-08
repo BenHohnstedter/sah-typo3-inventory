bc81a9d83114634af4329983e50b9d2e5307b8f0
bhohnstedter <bhohnstedter@gmail.com>
2025-09-08 10:52:23 +0200

    Add mvc fo pearl

---
+++ packages/sahiv/Classes/Controller/PearlController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace benh\sahiv\Controller;
+
+use benh\sahiv\Domain\Model\Pearl;
+use benh\sahiv\Domain\Repository\PearlRepository;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class PearlController extends ActionController
+{
+    public function __construct(
+        protected PearlRepository $pearlRepository,
+    ) {
+    }
+
+    public function listAction(): ResponseInterface
+    {
+        $pearls = $this->pearlRepository->findAll();
+
+        $this->view->assignMultiple([
+            'pearls' => $pearls,
+        ]);
+
+        return $this->htmlResponse();
+    }
+
+    public function newAction(): ResponseInterface
+    {
+        return $this->htmlResponse();
+    }
+
+    public function createAction(Pearl $pearl): ResponseInterface
+    {
+        $this->pearlRepository->add($pearl);
+
+        return $this->redirect('list');
+    }
+
+    public function editAction(?Pearl $pearl = null): ResponseInterface
+    {
+        return $this->htmlResponse();
+    }
+
+    public function updateAction(Pearl $pearl): ResponseInterface
+    {
+        $this->pearlRepository->update($pearl);
+
+        return $this->redirect('list');
+    }
+
+    public function deleteAction(Pearl $pearl): ResponseInterface
+    {
+        $this->pearlRepository->remove($pearl);
+
+        return $this->redirect('list');
+    }
+}
---
+++ packages/sahiv/Classes/Domain/Model/Pearl.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace benh\sahiv\Domain\Model;
+
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+class Pearl extends AbstractEntity
+{
+    /** @var string */
+    protected $title = '';
+
+    /** @var string */
+    protected $acronym = '';
+
+    /** @var ObjectStorage<FileReference> */
+    protected $images;
+
+    /** @var float */
+    protected $unitPrice = 0.0;
+
+    /** @var int */
+    protected $stock = 0;
+
+    /** @var float */
+    protected $size = 0.0;
+
+    /** @var ObjectStorage<ColorCp> */
+    protected $colorscp;
+
+    /** @var ObjectStorage<ColorTone> */
+    protected $colortones;
+
+    /** @var ObjectStorage<MaterialCp> */
+    protected $materialscp;
+
+    /** @var ObjectStorage<Shape> */
+    protected $shapes;
+
+    /** @var bool */
+    protected $archived = false;
+
+    /** @var bool */
+    protected $deleted = false;
+
+    /** @var bool */
+    protected $hidden = false;
+
+    /** @var string */
+    protected $notes = '';
+
+    public function __construct()
+    {
+        $this->images = new ObjectStorage();
+        $this->colorscp = new ObjectStorage();
+        $this->colortones = new ObjectStorage();
+        $this->materialscp = new ObjectStorage();
+        $this->shapes = new ObjectStorage();
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getAcronym(): string
+    {
+        return $this->acronym;
+    }
+
+    public function setAcronym(string $acronym): void
+    {
+        $this->acronym = $acronym;
+    }
+
+    public function getImages(): ObjectStorage
+    {
+        return $this->images;
+    }
+
+    public function setImages(ObjectStorage $images): void
+    {
+        $this->images = $images;
+    }
+
+    public function getUnitPrice(): float
+    {
+        return $this->unitPrice;
+    }
+
+    public function setUnitPrice(float $unitPrice): void
+    {
+        $this->unitPrice = $unitPrice;
+    }
+
+    public function getStock(): int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(int $stock): void
+    {
+        $this->stock = $stock;
+    }
+
+    public function getSize(): float
+    {
+        return $this->size;
+    }
+
+    public function setSize(float $size): void
+    {
+        $this->size = $size;
+    }
+
+    public function getColorscp(): ObjectStorage
+    {
+        return $this->colorscp;
+    }
+
+    public function setColorscp(ObjectStorage $colorscp): void
+    {
+        $this->colorscp = $colorscp;
+    }
+
+    public function getColortones(): ObjectStorage
+    {
+        return $this->colortones;
+    }
+
+    public function setColortones(ObjectStorage $colortones): void
+    {
+        $this->colortones = $colortones;
+    }
+
+    public function getMaterialscp(): ObjectStorage
+    {
+        return $this->materialscp;
+    }
+
+    public function setMaterialscp(ObjectStorage $materialscp): void
+    {
+        $this->materialscp = $materialscp;
+    }
+
+    public function getShapes(): ObjectStorage
+    {
+        return $this->shapes;
+    }
+
+    public function setShapes(ObjectStorage $shapes): void
+    {
+        $this->shapes = $shapes;
+    }
+
+    public function isArchived(): bool
+    {
+        return $this->archived;
+    }
+
+    public function setArchived(bool $archived): void
+    {
+        $this->archived = $archived;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->deleted;
+    }
+
+    public function setDeleted(bool $deleted): void
+    {
+        $this->deleted = $deleted;
+    }
+
+    public function isHidden(): bool
+    {
+        return $this->hidden;
+    }
+
+    public function setHidden(bool $hidden): void
+    {
+        $this->hidden = $hidden;
+    }
+
+    public function getNotes(): string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(string $notes): void
+    {
+        $this->notes = $notes;
+    }
+}

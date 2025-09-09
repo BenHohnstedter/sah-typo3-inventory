43dd7f8fb249c2fde4af3c95dd47628fe2b0b626
BenHohnstedter <bhohnstedter@gmail.com>
2025-09-09 19:02:06 +0200

    Add mvc for colorcp

---
+++ packages/sahiv/Classes/Controller/ColorcpController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace benh\sahiv\Controller;
+
+use benh\sahiv\Domain\Model\Colorcp;
+use benh\sahiv\Domain\Repository\ColorcpRepository;
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+
+class ColorcpController extends ActionController
+{
+    public function __construct(
+        protected ColorcpRepository $colorcpRepository,
+    ) {
+    }
+
+    public function listAction(): ResponseInterface
+    {
+        $colorcps = $this->colorcpRepository->findAll();
+
+        $this->view->assignMultiple([
+            'colorcps' => $colorcps,
+        ]);
+
+        return $this->htmlResponse();
+    }
+
+    public function newAction(): ResponseInterface
+    {
+        return $this->htmlResponse();
+    }
+
+    public function createAction(Colorcp $colorcp): ResponseInterface
+    {
+        $this->colorcpRepository->add($colorcp);
+
+        return $this->redirect('list');
+    }
+
+    public function editAction(?Colorcp $colorcp = null): ResponseInterface
+    {
+        return $this->htmlResponse();
+    }
+
+    public function updateAction(Colorcp $colorcp): ResponseInterface
+    {
+        $this->colorcpRepository->update($colorcp);
+
+        return $this->redirect('list');
+    }
+
+    public function deleteAction(Colorcp $colorcp): ResponseInterface
+    {
+        $this->colorcpRepository->remove($colorcp);
+
+        return $this->redirect('list');
+    }
+}
---
+++ packages/sahiv/Classes/Domain/Model/Colorcp.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace benh\sahiv\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+class Colorcp extends AbstractEntity
+{
+    /** @var string */
+    protected $title = '';
+
+    /** @var ObjectStorage<Charm> */
+    protected $charms;
+
+    /** @var ObjectStorage<Pearl> */
+    protected $pearls;
+
+    /** @var bool */
+    protected $hidden = false;
+
+    public function __construct()
+    {
+        $this->charms = new ObjectStorage();
+        $this->pearls = new ObjectStorage();
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return ObjectStorage<Charm>
+     */
+    public function getCharms(): ObjectStorage
+    {
+        return $this->charms;
+    }
+
+    public function setCharms(ObjectStorage $charms): void
+    {
+        $this->charms = $charms;
+    }
+
+    /**
+     * @return ObjectStorage<Pearl>
+     */
+    public function getPearls(): ObjectStorage
+    {
+        return $this->pearls;
+    }
+
+    public function setPearls(ObjectStorage $pearls): void
+    {
+        $this->pearls = $pearls;
+    }
+
+    public function isHidden(): bool
+    {
+        return $this->hidden;
+    }
+
+    public function setHidden(bool $hidden): void
+    {
+        $this->hidden = $hidden;
+    }
+}

cf55794a031fca30ee4d7dfd474b023d2b5caf9c
BenHohnstedter <bhohnstedter@gmail.com>
2025-12-22 08:56:18 +0100

    remove unnecessary inputs

--- packages/sahiv/Classes/Domain/Model/Materialcp.php
+++ packages/sahiv/Classes/Domain/Model/Materialcp.php
@@ -3,27 +3,14 @@
 namespace benh\sahiv\Domain\Model;
 
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
-use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
 class Materialcp extends AbstractEntity
 {
     /** @var string */
     protected $title = '';
 
-    /** @var ObjectStorage<Charm> */
-    protected $charms;
-
-    /** @var ObjectStorage<Pearl> */
-    protected $pearls;
-
     /** @var bool */
     protected $hidden = false;
-
-    public function __construct()
-    {
-        $this->charms = new ObjectStorage();
-        $this->pearls = new ObjectStorage();
-    }
 
     public function getTitle(): string
     {
@@ -33,32 +20,6 @@ class Materialcp extends AbstractEntity
     public function setTitle(string $title): void
     {
         $this->title = $title;
-    }
-
-    /**
-     * @return ObjectStorage<Charm>
-     */
-    public function getCharms(): ObjectStorage
-    {
-        return $this->charms;
-    }
-
-    public function setCharms(ObjectStorage $charms): void
-    {
-        $this->charms = $charms;
-    }
-
-    /**
-     * @return ObjectStorage<Pearl>
-     */
-    public function getPearls(): ObjectStorage
-    {
-        return $this->pearls;
-    }
-
-    public function setPearls(ObjectStorage $pearls): void
-    {
-        $this->pearls = $pearls;
     }
 
     public function isHidden(): bool
